8a82eecd8dfc737f9c52cb3cf7a96cec8f46a2f3
Ray Viljoen <rayviljoen@me.com>
2013-01-27 23:13:31 +0000

    Changes Kinetic for dev version

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var http = require('http'),
 ss.client.define('main', {
   view: 'app.jade',
   css:  ['app.styl'],
-  code: ['libs/jquery.min.js', 'libs/kinetic.min.js', 'app'],
+  code: ['libs/jquery.min.js', 'libs/kinetic.js', 'app'],
   tmpl: '*'
 });
 
